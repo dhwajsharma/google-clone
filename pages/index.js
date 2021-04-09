9edebf555f75b8a9dcbc2f7e9bb53e8248d2e4c0
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-04-09 16:59:50 +0530

    Done with Header

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,36 @@
 import Head from 'next/head'
+import Avatar from '../components/Avatar'
+import { MicrophoneIcon, ViewGridIcon } from "@heroicons/react/solid"
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Google</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h2>Dhwaj</h2>
+      {/* Header */}
+      <header className="flex w-full p-5 justify-between text-sm text-gray-700">
+        <div className="flex space-x-4 items-center">
+          <p className="link">About</p>
+          <p className="link">Store</p>
+        </div>
+
+        <div className="flex space-x-4 items-center">
+          <p className="link">Gmail</p>
+          <p className="link">Images</p>
+
+          <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" />
+
+          <Avatar url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0yrUl8sTPilwjvwq4dTho8duuzk7GIVHEWWKN5PYc7h-h7zW84Nc4bWv3Ts62S7KOvyM&usqp=CAU" />
+        </div>
+      </header>
+
+      {/* Body */}
+
+
+      {/* Footer */}
     </div>
   )
 }
