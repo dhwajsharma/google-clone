3dbfde3513acea92ed7692b8d5c913efe9d9d532
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-04-09 19:43:26 +0530

    Done with Body

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Avatar from '../components/Avatar'
 import { MicrophoneIcon, ViewGridIcon } from "@heroicons/react/solid"
+import { SearchIcon } from "@heroicons/react/outline"
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -27,7 +29,24 @@ export default function Home() {
         </div>
       </header>
 
-      {/* Body */}
+      <form className="flex flex-col items-center mt-40 flex-grow">
+        <Image
+          loading="lazy"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"
+          height={100}
+          width={300}
+        />
+        <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl ">
+          <SearchIcon className="h-5 mr-3 text-gray-500" />
+          <input type="text" className="flex-grow focus:outline-none" />
+          <MicrophoneIcon className="h-5" />
+        </div>
+
+        <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
+          <button className="btn">Google Search</button>
+          <button className="btn">I'm feeling lucky</button>
+        </div>
+      </form>
 
 
       {/* Footer */}
